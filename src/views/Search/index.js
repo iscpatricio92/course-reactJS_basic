7717192b7a846c8a11a50f6e20f2ea9ac093f9dc
Patricio Gomez <pat@Patricios-MacBook-Pro.local>
2024-03-22 14:40:17 -0600

    with axios

--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import SearchBox from "./components/SearchBox";
+import axios from "axios";
 import "./style.css";
 import SearchResults from "./components/SearchResults";
 export default function Search() {
@@ -9,11 +10,24 @@ export default function Search() {
 
   useEffect(() => {
     const getUsers = async () => {
-      const response = await fetch(
+      /**WITH FETCH */
+      /*const response = await fetch(
         "https://jsonplaceholder.typicode.com/users"
       );
       const data = await response.json();
       setData(data);
+      getUsers().catch(null);
+      */
+
+      /**WITH AXIOS */
+      try {
+        const { data } = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setData(data);
+      } catch (err) {
+        console.error(err);
+      }
     };
     getUsers().catch(null);
   }, []);
